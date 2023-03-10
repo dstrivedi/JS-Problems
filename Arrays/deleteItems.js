67771e856838e17c1d995ccd45c9491eb0fc8df8
Drashti Trivedi <dstrivedi@users.noreply.github.com>
2023-03-10 11:45:51 +0000

    few more problems on Arrays

---
+++ Arrays/deleteItems.js
@@ -0,0 +1,10 @@
+// Different ways to delete an item from an array using JavaScript
+
+/*
+  pop() - removes last element
+  shift() - removes first elements
+  filter() - removes based on filter cond
+  splice() - Array.splice( index, remove_count, item_list )
+  delete - delete object or delete object.property or delete object['property']
+*/
+
---
+++ Arrays/diffWaysToCloneArray.js
@@ -0,0 +1,13 @@
+const array_clone = (input) => {
+  return input.slice(0)
+}
+
+let arr = [1, 2, 4, 0]
+let arr3 = [...arr]
+let arr4 = arr
+let arr5 = Array.from(arr)
+let arr2 = array_clone([1, 2, 4, 0]);
+console.log(arr2)
+arr.push(5)
+console.log(arr, arr2, arr3, arr4, arr5)
+
